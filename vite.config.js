6b2a05d1f3241804edcbc6c90369852e144ff679
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2022-03-25 13:29:07 +0100

    fix: vite config related to hybrid package support

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,21 @@ const path = require("path");
 const { defineConfig } = require("vite");
 
 const getPackageName = () => {
+    return packageJson.name;
+};
+
+const getPackageNameCamelCase = () => {
   try {
-    return packageJson.name.replace(/-./g, (char) => char[1].toUpperCase());
+    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
   } catch (err) {
     throw new Error("Name property in package.json is missing.");
   }
+};
+
+const fileName = {
+  es: `${getPackageName()}.mjs`,
+  cjs: `${getPackageName()}.cjs`,
+  iife: `${getPackageName()}.iife.js`,
 };
 
 module.exports = defineConfig({
@@ -15,10 +25,9 @@ module.exports = defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: getPackageName(),
+      name: getPackageNameCamelCase(),
       formats: ["es", "cjs", "iife"],
-      fileName: (format) =>
-        format === "es" ? "my-lib.mjs" : `my-lib.${format}.js`,
+      fileName: (format) => fileName[format]
     },
   },
 });
