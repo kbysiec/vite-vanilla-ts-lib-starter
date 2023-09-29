37b51c64e9a16dbcff5e9e203e7c8cfcb4314bf4
Manuel Bujes Garrido <manu.bujes@inari.io>
2023-09-29 17:48:50 +0200

    96b71f51cf6c137b44ff4fa78f667f190d6acf50

--- .releaserc.js
+++ .releaserc.js
@@ -1,13 +1,17 @@
 module.exports = {
-  "branches": ["master", "develop"],
-  "plugins": [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/github',
-    ["@semantic-release/git", {
-      "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }]
-  ]
-}
+  branches: ["master", "develop"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/**/*.{js,css}", "docs", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
