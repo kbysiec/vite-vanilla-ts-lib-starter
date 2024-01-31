90a9ca2afb222616b7d2c5e5736da283a967c607
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2024-01-31 14:24:03 +0100

    config: update building script to build directly to the build folder and copy package.json file

--- dts-bundle-generator.config.ts
+++ dts-bundle-generator.config.ts
@@ -1,15 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require("./package.json");
-
-const getPackageName = () => {
-  return packageJson.name;
-};
-
 const config = {
   entries: [
     {
       filePath: "./src/index.ts",
-      outFile: `./dist/${getPackageName()}.d.ts`,
+      outFile: "./build/dist/index.d.ts",
       noCheck: false,
     },
   ],
