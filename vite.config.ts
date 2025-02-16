01f8b59d3243c0ecacad581694ab8589f2f8827d
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2025-02-16 19:00:21 +0100

    config: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -37,9 +37,9 @@ export default defineConfig({
     watch: false,
   },
   resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") },
-      { find: "@@", replacement: path.resolve(__dirname) },
-    ],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@@": path.resolve(__dirname),
+    },
   },
 });
